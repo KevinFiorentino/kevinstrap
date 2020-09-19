d85fd1f24800b1f7b227f14073c8ce261df00cba
kevin <kevinfiorentino.1294@gmail.com>
2020-09-19 16:53:52 -0300

    Extraemos CSS del JS, directorio /fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/js/app.js',
@@ -14,42 +14,64 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 'css-loader'
+                    MiniCSSExtractPlugin.loader, 'css-loader'
                 ]
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader', 'css-loader', 'sass-loader'
+                    MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'
                 ]
             },
             {
                 test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                loader: [ 'file-loader' ]
+                use: [ 
+                    { 
+                        loader: 'file-loader', 
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    } 
+                ],
             },
             {
                 test: /\.(woff)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                loader: [ 'url-loader' ]
+                use: [ 
+                    { 
+                        loader: 'url-loader', 
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    } 
+                ],
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
+            template: './src/index.html',
+            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: 'contacto.html',
-            template: './src/contacto.html'
+            template: './src/contacto.html',
+            hash: true
         }),
         new HtmlWebpackPlugin({
             filename: 'precios.html',
-            template: './src/precios.html'
+            template: './src/precios.html',
+            hash: true
         }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
             Popper: ['popper.js', 'default'],
+        }),
+        new MiniCSSExtractPlugin({
+            filename: "styles.css",
         })
     ],
     devServer: {
